2a98659aaba9acbd400c51bcba0c701744e1b33a
hugodct <hugodct@gmail.com>
2024-06-19 15:01:02 +0200

    cleaned libraries

--- BOEutils.py
+++ BOEutils.py
@@ -2,7 +2,6 @@ import PyPDF2
 import re, os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
